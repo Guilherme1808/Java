296f6290b07d09ff4c7701ea3d4743a27d547bab
Guilherme Soares <guilherme1808@hotmail.com>
2021-02-27 21:19:01 -0300

    Aula 28

---
+++ Loiane/curso-java-basico/aula28/Pacotes.java
@@ -0,0 +1,19 @@
+import aula27.Carro;
+
+import aula27.labs.*;
+import labs.Exer04;
+import labs.Exer05;
+import labs.Exer06;
+
+public class Pacotes {
+
+    public static void main(String[] args) {
+
+        Carro carro;
+        Carro carro2;
+
+        Exer06 exer06;
+        Exer05 exer05;
+        Exer04 exer04;
+    }
+}
